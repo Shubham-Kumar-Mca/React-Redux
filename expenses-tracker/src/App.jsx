c31f112862ea77f736d2a0ce4ae0dcd970aa68eb
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-04-24 23:54:18 +0530

    App.jsx file

---
+++ expenses-tracker/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/navbar/Navbar';
+import AllRoutes from "./pages/AllRoutes";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRoutes />
+    </div>
+  )
+}
+
+export default App
